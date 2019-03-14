e401a2b93dffa659bddf035f8dcb4abc9b1f61c7
James Swank <jmswank7@gmail.com>
2019-03-14 13:55:20 -0600

    fixes current decade year indexer

--- practice3.py
+++ practice3.py
@@ -56,8 +56,9 @@ df10 = df_da[0:-1]
 # current_year_index = current_year[-1]
 print(current_year)
 current_year_decade = current_year%10
-print(current_year_decade)
-df_da_cd = (df5[-(current_year_decade):]).mean()
+current_year_indexer = current_year_decade + 1
+print(current_year_indexer)
+df_da_cd = (df5[-(current_year_indexer):]).mean()
 df_da_cd['combined'] = (df_da_cd['TMAX'] + df_da_cd['TMIN']) / 2
 df5['combined'] = (df5['TMAX'] + df5['TMIN']) / 2
 df10['combined'] = (df10['TMAX'] + df10['TMIN']) / 2
