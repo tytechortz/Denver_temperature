534f65b0cb1cc236e1983d55f0c0562f282b8da4
James Swank <jmswank7@gmail.com>
2019-03-15 20:28:46 -0600

    adds table

--- practice4.py
+++ practice4.py
@@ -34,19 +34,52 @@ df_old = pd.read_csv('./stapleton.csv').round(1)
 df_new = pd.read_csv('https://www.ncei.noaa.gov/access/services/data/v1?dataset=daily-summaries&dataTypes=TMAX,TMIN&stations=USW00023062&startDate=2019-01-01&endDate=' + today + '&units=standard').round(1)
 df = pd.concat([df_old, df_new], ignore_index=True)
 
-df['datetime'] = pd.to_datetime(df['DATE'])
-df = df.set_index('datetime')
-print(df)
+df['DATE'] = pd.to_datetime(df['DATE'])
+df = df.set_index('DATE')
 
+df_ya_max = df.resample('Y').mean()
+
+df.loc[:,'TAVG'] = ((df.TMAX + df.TMIN) / 2)
+
+# record high and low
+record_max = df.loc[df['TMAX'].idxmax()]
+record_min = df.loc[df['TMIN'].idxmin()]
+
+df_ya_max = df.resample('Y').mean()
+df_da = df_ya_max.groupby((df_ya_max.index.year//10)*10).mean()
+
+# removes final year in df
+df5 = df_ya_max[:-1]
+# removes final decade in dacade averages
+df10 = df_da[0:-1]
+
+# filters for completed years in current decade
+current_year_decade = current_year%10
+current_year_indexer = current_year_decade + 1
+# current year decade avg current decade
+df_da_cd = (df5[-(current_year_indexer):]).mean()
+# add current decade to decade list
+df10.loc['2010'] = df_da_cd
+df10 = df10.round(1)
+df10 = df10.reset_index()
+print(df10)
 # year list for dropdown selector
 year = []
 for YEAR in df.index.year.unique():
     year.append({'label':(YEAR), 'value':YEAR})
 
-
-
 body = dbc.Container([
-
+    dbc.Row([
+        dbc.Col([
+            dash_table.DataTable(
+                data=df10.to_dict('rows'),
+                columns=[{'id': c, 'name': c} for c in df10.columns]
+            ),
+        ]),
+        dbc.Col([
+            html.H3('stuff')
+        ])
+    ]),
 ])
 
 
@@ -54,9 +87,6 @@ body = dbc.Container([
               [Input('year-picker', 'value')])
 def update_layout_a(selected_year):
     return 'Stats for {}'.format(selected_year)
-
-
-    
 
 
 
